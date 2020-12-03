71710d1678dd5351bf4659580de5234e052fdcd6
darrahts <timothy.s.darrah@vanderbilt.edu>
2020-12-02 19:53:22 -0600

    added powertrain.py

--- systems/__init__.py
+++ systems/__init__.py
@@ -1,4 +1,5 @@
 from .cartpole import CartPoleEnv, CartPoleDataEnv, plot_cartpole
 from .cartpole_continuous import CartPoleContinuousEnv, plot_cartpole_continuous
-from .tanks import TanksPhysicalEnv, TanksDataEnv, TanksFactory, plot_tanks
+#from .tanks import TanksPhysicalEnv, TanksDataEnv, TanksFactory, plot_tanks
 from .lunarlander import LunarLanderEnv, random_lunarlander, plot_lunarlander
+from .powertrain import *
